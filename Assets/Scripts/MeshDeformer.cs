cdea2d621888564b51255f321585e37a45623a8c
Bryan Ma <bryanhma@gmail.com>
2017-10-27 17:53:21 -0400

    first

---
+++ Assets/Scripts/MeshDeformer.cs
@@ -0,0 +1,81 @@
+﻿using UnityEngine;
+
+[RequireComponent(
+  typeof(MeshFilter)
+)]
+
+public class MeshDeformer : MonoBehaviour {
+
+  public Transform transform;
+
+  public float springForce = 20f;
+  public float damping = 5f;
+  private float uniformScale = 1f;
+
+  private Mesh deformingMesh;
+  private MeshCollider colliderMesh;
+  private Vector3[] originalVertices, displacedVertices;
+  private Vector3[] vertexVelocities;
+
+	private void Start() {
+    transform = GetComponent<Transform>();
+
+		deformingMesh = GetComponent<MeshFilter>().mesh;
+    colliderMesh = GetComponent<MeshCollider>();
+    originalVertices = deformingMesh.vertices;
+    displacedVertices = new Vector3[originalVertices.Length];
+    for (int i = 0; i < originalVertices.Length; i++) {
+      displacedVertices[i] = originalVertices[i];
+    }
+    vertexVelocities = new Vector3[originalVertices.Length];
+	}
+
+  public void AddDeformingForce(Vector3 point, float force) {
+    Debug.DrawLine(Camera.main.transform.position, point);
+    point = transform.InverseTransformPoint(point);
+
+    for (int i = 0; i < displacedVertices.Length; i++) {
+      AddForceToVertex(i, point, force);
+    }
+  }
+
+  private void AddForceToVertex(int i, Vector3 point, float force) {
+    Vector3 pointToVertex = displacedVertices[i] - point;
+    pointToVertex *= uniformScale;
+    float attenuatedForce = force / (1f + pointToVertex.sqrMagnitude);
+    float velocity = attenuatedForce * Time.deltaTime;
+    vertexVelocities[i] += pointToVertex.normalized * velocity;
+  }
+
+  private void FixedUpdate() {
+    transform.Rotate(Vector3.up * 40f * Time.deltaTime);
+  }
+
+  private void Update() {
+
+    if (Input.GetMouseButton(0)) {
+      springForce = 20f;
+    } else {
+      springForce = 0f;
+    }
+
+    uniformScale = transform.localScale.x;
+    for (int i = 0; i < displacedVertices.Length; i++) {
+      UpdateVertex(i);
+    }
+    deformingMesh.vertices = displacedVertices;
+    colliderMesh.sharedMesh = deformingMesh;
+    deformingMesh.RecalculateNormals();
+  }
+
+  private void UpdateVertex(int i) {
+    Vector3 velocity = vertexVelocities[i];
+    Vector3 displacement = displacedVertices[i] - originalVertices[i];
+    displacement *= uniformScale;
+    velocity -= displacement * springForce * Time.deltaTime;
+    velocity *= 1f - damping * Time.deltaTime;
+    vertexVelocities[i] = velocity;
+    displacedVertices[i] += velocity * (Time.deltaTime / uniformScale);
+    originalVertices[i] = displacedVertices[i];   // don't need original vs. displaced if i don't actually return to starting position
+  }
+}
---
+++ Assets/Scripts/MeshDeformerInput.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+public class MeshDeformerInput : MonoBehaviour {
+
+  public float force = 10f;
+  public float forceOffset = 0.1f;
+
+	private void Start() {
+		
+	}
+	
+	private void Update() {
+		if (Input.GetMouseButton(0)) {
+      HandleInput();
+    }
+	}
+
+  private void HandleInput() {
+    Ray inputRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+    RaycastHit hit;
+
+    if (Physics.Raycast(inputRay, out hit)) {
+      MeshDeformer deformer = hit.collider.GetComponent<MeshDeformer>();
+      if (deformer) {
+        Vector3 point = hit.point;
+        point += hit.normal * forceOffset;
+        deformer.AddDeformingForce(point, force);
+      }
+    }
+  }
+}
